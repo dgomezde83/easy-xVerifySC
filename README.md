bfbdb97034dcd550f6424a05565f7e03bf9abd80
dgomezde83 <dgomezde83@gmail.com>
2023-12-19 01:54:16 +0100

    Improving readme and script

--- README.md
+++ README.md
@@ -1,36 +1,40 @@
 # easy-xVerifySC
-**Seamlessly verify a smart contract on the MultiversX devnet or mainnet.**
+**Seamlessly deploy and verify a smart contract on the MultiversX devnet or mainnet.**
 
 ## How to Use?
 
 ### Step 1: Install Docker
 1. **Install Docker** following the [Generic Installation Steps](https://docs.docker.com/desktop/install/linux-install/).
 
-### Step 2: Pull image
-Pull the latest image version from [DockerHub(https://hub.docker.com/r/multiversx/sdk-rust-contract-builder).
+### Step 2: Run the Verification Script
+There are two ways to run the script: either in "simulate" mode or in normal mode. The "simulate" mode will not deploy the SC to the devnet/mainnet but will only simulate its deployment to verify that everything is functioning correctly before actual deployment.
 
-### Step 3: Prepare Your Wallet File
-2. Place your wallet file (`.pem` or `.json`) adjacent to the `verify.py` file for streamlined usage.
+   ```bash
+   python verify.py simulate
 
-### Step 4: Run the Verification Script
-3. Execute the script using the command:
    ```bash
    python verify.py
 
 The script will prompt you for several pieces of information:
 
-1. **"Project Directory":**  Specify the root directory where the smart contract source code is located.
+1. **"Project Directory":** Specify the root directory where the smart contract source code is located.
 
 2. **"Build Output Directory":**  Indicate the output directory for storing build information (note: this directory should not exist prior to running the script; it will be created automatically).
 
-3. **"Image Version":**  Enter the latest image version from [DockerHub(https://docs.docker.com/desktop/install/linux-install/).
+3. **"Image Version":**  Enter the latest image version from [DockerHub](https://docs.docker.com/desktop/install/linux-install/).
+
+After this, the script will create a reproducible build for the specified image. Then, two additional pieces of information will be requested:
 
 4. **"Network of the Smart Contract (D or M)":**  Specify 'D' for Devnet or 'M' for Mainnet.
 
-After this, the script will create the reproducible build for the specified image. A prompt will ask you to deploy the newly created wasm file (you will have to do this yourself).
+5. **"Wallet File Name (PEM or JSON)":**   Enter the name of the wallet file with which you are going to deploy and verify your contract.
 
-Two more pieces of information will be asked to you:
+A prompt will ask you to deploy the newly created wasm file to the chosen network. The script will suggest an mxpy command to deploy a payable, non-upgradable, readable contract. You are free to copy and paste the suggested command template into another terminal to deploy the contract as you like.
 
-1. **"Smart Contract Address":**  Provide the address of the deployed smart contract.
+One more piece of information will be requested to verify the contract:
 
-2. **"Wallet File Name (PEM or JSON)":**  Enter the name of the wallet file from which the smart contract was deployed.
+6. **"Smart Contract Address":**  Provide the address of the deployed smart contract.
+
+Now, the verification process will start. It may take a few minutes to complete, and another few minutes to appear on the chain. Be patient!
+
+Enjoy :)
---
+++ build_and_verify.py
@@ -0,0 +1,183 @@
+#!/usr/bin/env python3
+import os
+import readline
+import subprocess
+import sys
+import glob
+import argparse
+
+def get_args():
+    parser = argparse.ArgumentParser(description="Build and verify script")
+    parser.add_argument("simulate", nargs='?', help="Run in simulation mode", default="")
+    return parser.parse_args()
+
+def check_command_installed(command, version_flag="--version"):
+    try:
+        subprocess.run([command, version_flag], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        return True
+    except subprocess.CalledProcessError:
+        return False
+
+def complete(text, state):
+    options = [x for x in os.listdir('.') if x.startswith(text)]
+    try:
+        return options[state]
+    except IndexError:
+        return None
+
+def get_input(prompt):
+    readline.set_completer_delims(' \t\n=')
+    readline.parse_and_bind("tab: complete")
+    readline.set_completer(complete)
+    return input(prompt)
+
+def validate_smart_contract_address(address):
+    return address.startswith("erd1") and len(address) == 62
+
+def find_script_file(build_output_dir):
+    search_path = os.path.join(build_output_dir, "*", "*.source.json")
+    files = glob.glob(search_path)
+    return files[0] if files else None
+    
+def find_bytecode_file(build_output_dir):
+    search_path = os.path.join(build_output_dir, "*", "*.wasm")
+    files = glob.glob(search_path)
+    return files[0] if files else None
+    
+def confirm_execution(command_description):
+    confirm = input(f"Do you want to execute the {command_description}? (yes/no): ").strip().lower()
+    return confirm in ["yes", "y"]
+    
+def is_docker_image_available(image_name):
+    try:
+        result = subprocess.run(["sudo","docker", "image", "inspect", image_name], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        return result.returncode == 0
+    except subprocess.CalledProcessError:
+        return False
+
+def pull_docker_image(image_name):
+    print(f"Pulling Docker image {image_name}...")
+    subprocess.run(["sudo","docker", "pull", image_name], check=True)
+
+def main(args=None):
+    if not check_command_installed("docker"):
+        sys.exit("Docker is not installed. Please install Docker and try again.")
+
+    if not check_command_installed("mxpy"):
+        sys.exit("mxpy is not installed. Please install mxpy and try again.")
+        
+    if args is None:
+        args = get_args()
+    
+    project_dir = get_input("Enter the project directory (relative to current dir): ")
+    build_output_dir = get_input("Enter the build output directory (relative to current dir): ")
+    build_output_path = os.path.join(os.getcwd(), build_output_dir)
+    if not os.path.isdir(build_output_path):
+        print(f"Build output directory does not exist. Creating directory: {build_output_path}")
+        os.makedirs(build_output_path)
+        
+    image_version = get_input("Enter the image version (e.g., v5.3.0): ")
+    image_name = f"multiversx/sdk-rust-contract-builder:{image_version}"    
+    if not is_docker_image_available(image_name):
+        download_image = input(f"The Docker image '{image_name}' is not available locally. Do you want to download it? (yes/no): ").strip().lower()
+        if download_image in ["yes", "y"]:
+            pull_docker_image(image_name)
+        else:
+            print("Docker image is required to proceed. Exiting.")
+            sys.exit(1)
+            
+    project_path = os.path.join(os.getcwd(), project_dir)
+    
+
+    # Running the Docker build command
+    build_command = [
+        "sudo", "python3", "./build_with_docker.py",
+        "--image", image_name,
+        "--project", project_path,
+        "--output", build_output_path
+    ]
+
+    print("Executing command: " + ' '.join(build_command))
+    if confirm_execution("Docker build command"):        
+        subprocess.run(build_command)
+    else:
+        print("Skipping Docker build command.")
+
+    # Find the script file
+    script_file = find_script_file(build_output_path)
+    if not script_file:
+        print("Error: No '.source.json' file found in the build output directory.")
+        return
+    
+    network = get_input("Enter the network of the smart contract (D or M): ")
+    while network not in ['D', 'M']:
+        print("Invalid network. Please enter 'D' for Devnet or 'M' for Mainnet.")
+        network = get_input("Enter the network of the smart contract (D or M): ")
+        
+    wallet = get_input("Enter the wallet file name (relative to current dir) (with .pem or .json extension): ")
+    while not wallet.endswith('.pem') and not wallet.endswith('.json'):
+        print("Invalid wallet file. The extension must be .pem or .json.")
+        wallet = get_input("Enter the wallet file name (relative to current dir) (with .pem or .json extension): ")
+
+    # Construct wallet file path
+    wallet_file_path = os.path.join(os.getcwd(), wallet)
+
+    # Construct wallet command
+    wallet_command = f"--pem" if wallet.endswith('.pem') else f"--keyfile"
+    
+    # Proxy URL address
+    proxy_url = "https://devnet-gateway.multiversx.com" if network == 'D' else "https://gateway.multiversx.com"
+
+    # Ask the user to deploy the contract
+    print(f"Please deploy the smart contract created in '{build_output_path}' to {'Mainnet' if network == 'M' else 'Devnet'}.")
+    bytecode_file = find_bytecode_file(build_output_path)
+    if not bytecode_file:
+        print("Error: No '.wasm' file found in the build output directory.")
+        return
+
+    # Determine the chain value based on the network
+    chain_value = "D" if network == "D" else "1"
+    send_simulate_flag = "--simulate" if args.simulate == "simulate" else "--send"
+
+    deploy_command = [
+        "mxpy", "--verbose", "contract", "deploy", "--recall-nonce", "--metadata-not-upgradeable", "--metadata-payable",
+        "--bytecode", bytecode_file,
+        wallet_command, wallet_file_path,
+        "--gas-limit", "60000000",
+        "--proxy", proxy_url, "--chain", chain_value,
+        send_simulate_flag
+    ]
+
+
+    print("You can deploy the contract using the following command: " + ' '.join(deploy_command))
+    if confirm_execution("mxpy contract deployment command"):        
+        subprocess.run(deploy_command)
+    else:
+        print("Skipping contract deployment.")
+    
+    verifier_url = "https://devnet-play-api.multiversx.com" if network == 'D' else "https://play-api.multiversx.com"
+
+    smart_contract_address = get_input("Enter the smart contract address: ")
+    while not validate_smart_contract_address(smart_contract_address):
+        print("Invalid smart contract address. It should start with 'erd1' and be 62 characters long.")
+        smart_contract_address = get_input("Enter the smart contract address: ")
+
+    # Running the mxpy command
+    mxpy_command = [
+        "mxpy", "--verbose", "contract", "verify", smart_contract_address,
+        "--packaged-src",script_file, "--verifier-url",verifier_url,
+        "--docker-image",image_name, wallet_command, wallet_file_path,
+    ]
+
+    #Check user if he wants to skip this command
+    print("Executing command: " + ' '.join(mxpy_command))
+    if confirm_execution("mxpy verification command"):        
+        subprocess.run(mxpy_command)
+    else:
+        print("Skipping mxpy verification command.")
+
+    # Ensure that the function always returns the command strings
+    return ' '.join(build_command), ' '.join(mxpy_command)
+
+if __name__ == "__main__":
+    main()
